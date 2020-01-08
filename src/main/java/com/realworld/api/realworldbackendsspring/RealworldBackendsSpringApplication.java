fd46a96e494e8acd3068070ea629846354a51a9e
이진호 <jinholee@nts-corp.com>
2020-01-08 18:07:08 +0900

    프로젝트 생성

---
+++ src/main/java/com/realworld/api/realworldbackendsspring/RealworldBackendsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.realworld.api.realworldbackendsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldBackendsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealworldBackendsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/realworld/api/realworldbackendsspring/RealworldBackendsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realworld.api.realworldbackendsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldBackendsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
